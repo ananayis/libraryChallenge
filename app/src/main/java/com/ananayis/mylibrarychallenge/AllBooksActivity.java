27d398d6f0005784150ae8f103eda03398cdf5c8
Parinaz Soltani <p.soltani007@gmail.com>
2022-01-06 22:37:18 +0100

    add all book page. create adapter.

--- app/src/main/java/com/ananayis/mylibrarychallenge/AllBooksActivity.java
+++ app/src/main/java/com/ananayis/mylibrarychallenge/AllBooksActivity.java
@@ -1,14 +1,36 @@
 package com.ananayis.mylibrarychallenge;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class AllBooksActivity extends AppCompatActivity {
+
+    private RecyclerView booksRecView;
+    private BookRecViewAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_books);
+
+        adapter = new BookRecViewAdapter(this);
+
+        booksRecView = findViewById(R.id.booksRecView);
+
+        ArrayList<Book> books = new ArrayList<>();
+        books.add(new Book(1, "Ansichten eines Clowns", " Heinrich Theodor Böll", 353,"https://m.media-amazon.com/images/I/41w5XkpxO9L.jpg",
+                "This book is a novel by West German writer Heinrich Böll.","Following publication in 1963, the novel generated polemics in the press for its negative portrayal of the Catholic Church and the CDU party. Böll's liberal views on religion and social issues inspired the wrath of conservatives in Germany.[1] The conservative press even attacked Böll's 1972 Nobel prize award, arguing that it was awarded only to \"liberals and left-wing radicals."));
+
+        adapter.setBooks(books);
+
+        booksRecView.setAdapter(adapter);
+        booksRecView.setLayoutManager(new GridLayoutManager(this, 2));
+
+
     }
 }
---
+++ app/src/main/java/com/ananayis/mylibrarychallenge/BookRecViewAdapter.java
@@ -0,0 +1,78 @@
+package com.ananayis.mylibrarychallenge;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager2.adapter.FragmentViewHolder;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+
+public class BookRecViewAdapter extends RecyclerView.Adapter<BookRecViewAdapter.ViewHolder> {
+
+    private ArrayList<Book> books = new ArrayList<>();
+    private Context mContext;
+
+    public BookRecViewAdapter(Context mContext) {
+        this.mContext = mContext;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item_book, parent, false);
+        ViewHolder holder = new ViewHolder(view);
+
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.txtName.setText(books.get(position).getName());
+        Glide.with(mContext)
+                .asBitmap()
+                .load(books.get(position).getImageURL())
+                .into(holder.image);
+
+        holder.parent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(mContext,books.get(holder.getAdapterPosition()).getName() + "Selected", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return books.size();
+    }
+
+    public void setBooks(ArrayList<Book> books) {
+        this.books = books;
+        notifyDataSetChanged();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        private CardView parent;
+        private ImageView image;
+        private TextView txtName;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            parent = itemView.findViewById(R.id.parent);
+            image = itemView.findViewById(R.id.image);
+            txtName = itemView.findViewById(R.id.txtName);
+
+
+        }
+    }
+}
