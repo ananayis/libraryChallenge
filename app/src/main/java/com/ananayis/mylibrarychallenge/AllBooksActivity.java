f3fa62a4c7a19a86a17acb966dfbab30dd536d56
Parinaz Soltani <p.soltani007@gmail.com>
2022-01-11 14:29:52 +0100

    create up button
    learning about animation

--- app/src/main/java/com/ananayis/mylibrarychallenge/AllBooksActivity.java
+++ app/src/main/java/com/ananayis/mylibrarychallenge/AllBooksActivity.java
@@ -1,11 +1,13 @@
 package com.ananayis.mylibrarychallenge;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import java.util.ArrayList;
 
@@ -19,6 +21,10 @@ public class AllBooksActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_books);
 
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+//        overridePendingTransition(R.anim.slide_in, R.anim.slide_out);
+
         adapter = new BookRecViewAdapter(this,"allBooks");
 
         booksRecView = findViewById(R.id.booksRecView);
@@ -31,4 +37,22 @@ public class AllBooksActivity extends AppCompatActivity {
 
 
     }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                onBackPressed();
+                break;
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    //    @Override
+//    public void finish() {
+//        super.finish();
+//        overridePendingTransition(R.anim.slide_out, R.anim.slide_in);
+//    }
 }
