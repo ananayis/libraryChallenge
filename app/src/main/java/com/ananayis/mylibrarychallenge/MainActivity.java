9b1aa30cc7bf9927e211acd59d9cc74db544af19
Parinaz Soltani <p.soltani007@gmail.com>
2022-01-11 13:45:11 +0100

    create about dialog
    create about dialog and webView

--- app/src/main/java/com/ananayis/mylibrarychallenge/MainActivity.java
+++ app/src/main/java/com/ananayis/mylibrarychallenge/MainActivity.java
@@ -1,7 +1,9 @@
 package com.ananayis.mylibrarychallenge;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -55,6 +57,30 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, FavoriteActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        btnAbout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle(getString(R.string.app_name));
+                builder.setMessage("Design and develop by Ananayis at ananayis.org\n" + "You can check my Website for more application.");
+                builder.setNegativeButton("Dismiss", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                });
+                builder.setPositiveButton("Visit", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Intent intent = new Intent(MainActivity.this, WebsiteActivity.class);
+                        intent.putExtra("url" , "https://www.google.com/");
+                        startActivity(intent);
+                    }
+                });
+                builder.create().show();
             }
         });
 
